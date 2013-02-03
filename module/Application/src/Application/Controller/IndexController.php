46a851898e6ec9de02282b2c2bbb9be49cafa1e4
unknown <lucas.s.abreu@gmail.com>
2013-02-03 21:12:32 -0200

    Desafio Aula 4

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -26,4 +26,22 @@ class IndexController extends ActionController {
                 ));
     }
 
+    public function postAction() {
+
+        $post_id = $this->params()->fromRoute('id', null);
+
+        if (is_null($post_id))
+            throw new \Exception('É preciso informar um ID para visualizar o post.');
+
+        $post = $this->getTable('Application\Model\Post')->get($post_id);
+
+        $where = new \Zend\Db\Sql\Predicate\Predicate();
+        $comments = $this->getTable('Application\Model\Comment')->fetchAll(null, $where->equalTo('post_id', $post->id));
+
+        return new ViewModel(array(
+                    'post' => $post->toArray(),
+                    'comments' => $comments->toArray(),
+                ));
+    }
+
 }
--- module/Application/tests/src/Application/Controller/IndexControllerTest.php
+++ module/Application/tests/src/Application/Controller/IndexControllerTest.php
@@ -2,12 +2,9 @@
 
 namespace Application\Controller;
 
-use Core\Test\ControllerTestCase;
-use Application\Controller\IndexController;
 use Application\Model\Post;
-use Zend\Http\Request;
-use Zend\Stdlib\Parameters;
-use Zend\View\Renderer\PhpRenderer;
+use Application\Model\Comment;
+use Core\Test\ControllerTestCase;
 
 /**
  * @group Controller
@@ -67,6 +64,90 @@ class IndexControllerTest extends ControllerTestCase {
     }
 
     /**
+     * Testa o funcionamento da tela de detalhe de um Post
+     */
+    public function testPostAction() {
+        $post = $this->addPost();
+
+        $comment0 = $this->addComment($post->id);
+        $comment1 = $this->addComment($post->id);
+
+        $this->routeMatch->setParam('action', 'post');
+        $this->routeMatch->setParam('id', 1);
+
+        $result = $this->controller->dispatch($this->request, $this->response);
+
+        // Verifica o Response
+        $response = $this->controller->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // Verifica o resultado do dispatch
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
+
+        $variables = $result->getVariables();
+
+        // Verifica se foi enviada a variavel 'post'
+        $this->assertArrayHasKey('post', $variables);
+
+        // Verifica se foi enviada a variável 'comments'
+        $this->assertArrayHasKey('comments', $variables);
+
+        $this->assertEquals($post->title, $variables['post']['title']);
+
+        $this->assertEquals(2, count($variables['comments']));
+
+        $this->assertEquals($comment0->id, $variables['comments'][0]['id']);
+        $this->assertEquals($comment0->post_id, $variables['comments'][0]['post_id']);
+        $this->assertEquals($comment0->description, $variables['comments'][0]['description']);
+
+        $this->assertEquals($comment1->id, $variables['comments'][1]['id']);
+        $this->assertEquals($comment1->post_id, $variables['comments'][1]['post_id']);
+        $this->assertEquals($comment1->description, $variables['comments'][1]['description']);
+    }
+
+    /**
+     * Testa se existe tratamento para a inexistencia de um id no parametro.
+     * @expectedException Exception
+     * @expectedExceptionMessage É preciso informar um ID para visualizar o post.
+     */
+    public function testIfNotInformAId() {
+        $this->routeMatch->setParam('action', 'post');
+        $this->controller->dispatch($this->request, $this->response);
+    }
+
+    /**
+     * Testa se é lançado erro ao informar ID inválido.
+     * @expectedException Exception
+     * @expectedExceptionMessage Could not find row 2
+     */
+    public function testThrowExceptionOnInvalidIdInformed() {
+        $this->routeMatch->setParam('action', 'post');
+        $this->routeMatch->setParam('id', '2');
+        $this->controller->dispatch($this->request, $this->response);
+    }
+
+    /**
+     * Testa se esta trazendo os comentários corretamente.
+     */
+    public function testCorrectLoadOfComments() {
+        $post0 = $this->addPost();
+        $post1 = $this->addPost();
+
+        $comment0 = $this->addComment($post0->id);
+        $comment1 = $this->addComment($post0->id);
+
+        $this->routeMatch->setParam('action', 'post');
+        $this->routeMatch->setParam('id', '2');
+        $result = $this->controller->dispatch($this->request, $this->response);
+
+        $variables = $result->getVariables();
+
+        $this->assertArrayHasKey('comments', $variables);
+
+        $this->assertEquals(0, count($variables['comments']));
+    }
+
+    /**
      * Adiciona um post para os testes
      */
     private function addPost() {
@@ -78,6 +159,18 @@ class IndexControllerTest extends ControllerTestCase {
         $saved = $this->getTable('Application\Model\Post')->save($post);
 
         return $saved;
+    }
+
+    private function addComment($post_id) {
+        $comment = new Comment();
+        $comment->post_id = $post_id;
+        $comment->description = 'Comentário importante <script>alert("ok");</script> <br> ';
+        $comment->name = 'Elton Minetto';
+        $comment->email = 'eminetto@coderockr.com';
+        $comment->webpage = 'http://www.eltonminetto.net';
+        $comment->comment_date = date('Y-m-d H:i:s');
+
+        return $this->getTable('Application\Model\Comment')->save($comment);
     }
 
 }
