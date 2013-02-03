703bd3a70bd0ced9a74eaedd5d415ab7600dacb9
unknown <lucas.s.abreu@gmail.com>
2013-02-03 18:33:19 -0200

    Aula 4

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,21 +1,29 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Core\Controller\ActionController;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        return new ViewModel();
+/**
+ * Controlador que gerencia os posts
+ * 
+ * @category Application
+ * @package Controller
+ * @author  Elton Minetto <eminetto@coderockr.com>
+ */
+class IndexController extends ActionController {
+
+    /**
+     * Mostra os posts cadastrados
+     * @return Zend\View\Model\ViewModel
+     */
+    public function indexAction() {
+        return new ViewModel(array(
+                    'posts' => $this->getTable('Application\Model\Post')
+                            ->fetchAll()
+                            ->toArray()
+                ));
     }
+
 }
