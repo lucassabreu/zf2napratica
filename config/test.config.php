00ac1ac54e2d42fbf06aaf48e60d4e420696ae6f
unknown <lucas.s.abreu@gmail.com>
2013-02-03 16:43:22 -0200

    Aula 3

--- config/test.config.php
+++ config/test.config.php
@@ -2,11 +2,12 @@
 
 return array(
     'db' => array(
-        'driver' => 'PDO_SQLite',
-        'dsn' => 'sqlite::memory:',
+        'driver' => 'PDO',
+        'dsn' => 'mysql:dbname=zf2napratica_test;host=localhost',
+        'username' => 'zend',
+        'password' => 'zend',
         'driver_options' => array(
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        )
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
     )
-
 );
