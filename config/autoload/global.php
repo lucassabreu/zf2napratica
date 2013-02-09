d2fcd3abe1db15c8b1ed2fdbc0cffd08f7944832
unknown <lucas.s.abreu@gmail.com>
2013-02-09 11:52:36 -0200

    Aula 5

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,12 +14,11 @@
 return array(
     'service_manager' => array(
         'factories' => array(
-            'Zend\Db\Adapter\Adapter'
-            => 'Zend\Db\Adapter\AdapterServiceFactory',
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
         ),
     ),
     'db' => array(
-        'driver' => 'Pdo',
+        'driver' => 'PDO',
         'dsn' => 'mysql:dbname=zf2napratica;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -2,50 +2,48 @@
 
 // module/Admin/conﬁg/module.config.php:
 return array(
-    'controllers' => array( //add module controllers
+    'controllers' => array(//add module controllers
         'invokables' => array(
             'Admin\Controller\Index' => 'Admin\Controller\IndexController',
         ),
     ),
-
     'router' => array(
         'routes' => array(
             'admin' => array(
-                'type'    => 'Literal',
+                'type' => 'Literal',
                 'options' => array(
-                    'route'    => '/admin',
+                    'route' => '/admin',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Admin\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                        'module'        => 'admin'
+                        'controller' => 'Index',
+                        'action' => 'index',
+                        'module' => 'admin'
                     ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type'    => 'Segment',
+                        'type' => 'Segment',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
+                            'route' => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
                             'defaults' => array(
                             ),
                         ),
-                        'child_routes' => array( //permite mandar dados pela url 
+                        'child_routes' => array(//permite mandar dados pela url 
                             'wildcard' => array(
                                 'type' => 'Wildcard'
                             ),
                         ),
                     ),
-                    
                 ),
             ),
         ),
     ),
-    'view_manager' => array( //the module can have a specific layout
+    'view_manager' => array(//the module can have a specific layout
         // 'template_map' => array(
         //     'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
         // ),
@@ -53,11 +51,15 @@ return array(
             'admin' => __DIR__ . '/../view',
         ),
     ),
-//    'db' => array( //module can have a specific db configuration
-//        'driver' => 'PDO_SQLite',
-//        'dsn' => 'sqlite:' . __DIR__ .'/../data/admin.db',
-//        'driver_options' => array(
-//            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-//        )
-//    )
+    'service_manager' => array(
+        'factories' => array(
+            'Session' => function($sm) {
+                return new Zend\Session\Container('ZF2napratica');
+            },
+            'Admin\Service\Auth' => function($sm) {
+                $dbAdapter = $sm->get('DbAdapter');
+                return new Admin\Service\Auth($dbAdapter);
+            },
+        )
+    ),
 );
