06e3c255d343b0b91cf6b196ef33c510a19bcceb
unknown <lucas.s.abreu@gmail.com>
2013-02-09 22:50:09 -0200

    Aula Form

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -5,6 +5,7 @@ return array(
     'controllers' => array(//add module controllers
         'invokables' => array(
             'Admin\Controller\Index' => 'Admin\Controller\IndexController',
+            'Admin\Controller\Auth' => 'Admin\Controller\AuthController',
         ),
     ),
     'router' => array(
