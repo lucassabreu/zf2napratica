9b7c65c092044023ec8781ea781586de4af8ed7c
unknown <lucas.s.abreu@gmail.com>
2013-02-10 16:48:13 -0200

    Aula ACL

--- module/Admin/Module.php
+++ module/Admin/Module.php
@@ -2,10 +2,9 @@
 
 namespace Admin;
 
-class Module
-{
-    public function getAutoloaderConfig()
-    {
+class Module {
+
+    public function getAutoloaderConfig() {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
                 __DIR__ . '/autoload_classmap.php',
@@ -18,8 +17,64 @@ class Module
         );
     }
 
-    public function getConfig()
-    {
+    public function getConfig() {
         return include __DIR__ . '/config/module.config.php';
     }
+
+    /**
+     * Executada no bootstrap do módulo
+     * 
+     * @param MvcEvent $e
+     */
+    public function onBootstrap($e) {
+        /** @var \Zend\ModuleManager\ModuleManager $moduleManager */
+        $moduleManager = $e->getApplication()->getServiceManager()->get('modulemanager');
+        /** @var \Zend\EventManager\SharedEventManager $sharedEvents */
+        $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
+
+        //adiciona eventos ao módulo
+        $sharedEvents->attach('Zend\Mvc\Controller\AbstractActionController', \Zend\Mvc\MvcEvent::EVENT_DISPATCH, array($this, 'mvcPreDispatch'), 100);
+    }
+
+    /**
+     * Verifica se precisa fazer a autorização do acesso
+     * @param  MvcEvent $event Evento
+     * @return boolean
+     */
+//    public function mvcPreDispatch($event) {
+//        $di = $event->getTarget()->getServiceLocator();
+//        $routeMatch = $event->getRouteMatch();
+//        $moduleName = $routeMatch->getParam('module');
+//        $controllerName = $routeMatch->getParam('controller');
+//
+//        if ($moduleName == 'admin' && $controllerName != 'Admin\Controller\Auth') {
+//            $authService = $di->get('Admin\Service\Auth');
+//            if (!$authService->authorize()) {
+//                $redirect = $event->getTarget()->redirect();
+//                $redirect->toUrl('/admin/auth');
+//            }
+//        }
+//        return true;
+//    }
+
+    /**
+     * Verifica se precisa fazer a autorização do acesso
+     * @param  MvcEvent $event Evento
+     * @return boolean
+     */
+    public function mvcPreDispatch($event) {
+        $di = $event->getTarget()->getServiceLocator();
+        $routeMatch = $event->getRouteMatch();
+        $moduleName = $routeMatch->getParam('module');
+        $controllerName = $routeMatch->getParam('controller');
+        $actionName = $routeMatch->getParam('action');
+
+        $authService = $di->get('Admin\Service\Auth');
+        if (!$authService->authorize($moduleName, $controllerName, $actionName)) {
+            throw new \Exception('Você não tem permissão para acessar este recurso');
+        }
+
+        return true;
+    }
+
 }
